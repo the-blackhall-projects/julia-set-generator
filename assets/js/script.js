ffc4c596466367c096c6601806b1b3aa7edce878
michael-megan <sba21652@student.cct.ie>
2023-04-08 02:22:16 +0100

    Display mouse pointer on canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,58 @@
 const canvas = document.getElementById("canvas");
 
+// Have canvas fill screen
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// Get context
 const ctx = canvas.getContext("2d");
+
+// Define a basic mouse structure
+const mouse = {
+	x: undefined,
+	y: undefined,
+}
+
+canvas.addEventListener("mousemove", function(event) {
+	mouse.x = event.x;
+	mouse.y = event.y;
+}) 
+
+class Pointer {
+	constructor() {
+		this.x = mouse.x;
+		this.y = mouse.y;
+
+	
+		this.size = 10;
+	}
+
+	update() {
+		this.x = mouse.x;
+		this.y = mouse.y;
+		
+	}
+	
+	draw() {
+		ctx.fillStyle = "blue";
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+		ctx.fill();
+	}
+}
+
+let pointer = new Pointer();
+
+
+function animate() {
+	ctx.clearRect(0,0, canvas.width, canvas.height);
+
+	pointer.update();
+	pointer.draw();
+
+
+
+	requestAnimationFrame(animate);
+}
+
+animate();

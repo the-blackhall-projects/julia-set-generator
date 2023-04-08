496d3bea46b25884838a22374d2b8768771c3456
michael-megan <sba21652@student.cct.ie>
2023-04-08 04:16:00 +0100

    Add functions to convert to and from screen coords

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,11 @@ const ctx = canvas.getContext("2d");
 
 // Have canvas resize
 function onWindowResize() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+
+	let minDimension = (window.innerWidth < window.innerHeight) ? window.innerWidth : window.innerHeight;
+	canvas.width = minDimension;
+	canvas.height = minDimension;
+	
 }
 window.addEventListener("resize", onWindowResize);
 onWindowResize();
@@ -19,6 +22,24 @@ const mouse = {
 	y: undefined,
 }
 
+// return complex number -2 < z < +2  from screen coordinates. 
+function convertFromScreenCoordinates(xPos, yPos) {
+	let minDimension = canvas.width;
+	let result = {};
+	result.x = xPos / minDimension * 4 - 2;	
+	result.y = 2 - yPos / minDimension * 4;
+	return result;
+}
+
+
+// return  screen coordinates from complex number -2 < z < +2  from
+function convertToScreenCoordinates(xC, yC) {
+	let minDimension = canvas.width;
+	let result = {};
+	result.x = (xC + 2) * minDimension / 4;	
+	result.y = (2 - yC) * minDimension / 4;
+	return result;
+}
 
 
 
@@ -45,6 +66,12 @@ class Pointer {
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
 		ctx.fill();
+		
+		let result = convertFromScreenCoordinates(this.x, this.y);
+		console.log("complex:", result.x, result.y );
+		let result2 = convertToScreenCoordinates(result.x, result.y);
+		console.log(canvas.width, canvas.height);
+		console.log("normal:",result2.x, result2.y);
 	}
 }
 
@@ -54,11 +81,22 @@ let pointer = new Pointer();
 function animate() {
 	ctx.clearRect(0,0, canvas.width, canvas.height);
 
+	let result = convertFromScreenCoordinates(mouse.x, mouse.y)
+	
+	document.getElementById("xpos").innerHTML = mouse.x;
+	document.getElementById("ypos").innerHTML = mouse.y;
+	document.getElementById("cx").innerHTML = result.x.toFixed(3);
+	document.getElementById("cy").innerHTML = result.y.toFixed(3);
+
+	let result2 = convertToScreenCoordinates(result.x, result.y)
+
+
+	document.getElementById("xpos2").innerHTML = result2.x.toFixed(3);
+	document.getElementById("ypos2").innerHTML = result2.y.toFixed(3);
+	
+	
 	pointer.update();
 	pointer.draw();
-
-
-
 	requestAnimationFrame(animate);
 }
 

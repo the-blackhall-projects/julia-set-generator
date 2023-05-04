c162df4350fbef533af0ab5a4550e14c7c472b2b
michael-megan <sba21652@student.cct.ie>
2023-05-04 22:16:29 +0100

    Add circular buffer class.

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,6 +102,38 @@ class Complex {
 }
 
 /**
+ * Fixed length circular buffer.  Once full, tail returns the oldest
+ * object to be inserted into the buffer.  Before buffer is full
+ * tail returns a null quantity.
+ */
+class Buffer {
+
+	constructor(length) {
+		this.head = 0;
+		this.length = length;
+		this.data = Array.apply(null, Array(length)).map(function () {});
+	}
+
+	insert(obj) {
+		this.data[this.head] = obj;
+		++this.head;
+		this.head = this.head % this.length;
+	}
+
+	get tail() {
+		let pointer = (this.head+this.length) % this.length;
+
+		let retVal = this.data[pointer];
+
+		if (typeof retVal === "undefined") {
+			return null;
+		} else {
+			return retVal;
+		}
+	}
+}
+
+/**
  * Holds a set of points in the Julia set based on reverse iteration.
  */
 class JuliaSetPoints {
